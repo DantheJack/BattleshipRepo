7bee6debff6f64771e2f8203dea67942b3b4629b
Tomro8 <38689975+Tomro8@users.noreply.github.com>
2019-01-13 16:28:00 +0100

    Function Compatibility: khbit
    khbit() is not available on Linux
    
    Found function implementation doing the same on Linux on http://www.flipcode.com/archives/_kbhit_for_Linux.shtml

--- boardtools.c
+++ boardtools.c
@@ -4,6 +4,15 @@
 #include "coordonnees.h"
 #include "boardtools.h"
 #include <curses.h>
+
+/**
+ Linux (POSIX) implementation of _kbhit().
+ Morgan McGuire, morgan@cs.brown.edu
+ */
+#include <sys/select.h>
+#include <termios.h>
+#include <stropts.h>
+
 #define tempssupp 200;
 
 int _kbhit(void);
@@ -266,7 +275,7 @@ char** placeShip(char** matrice, short nb_colonnes, short nb_lignes, short size_
         }
 
         //Boucle d'affichage des deux matrices pour clignotement
-        while (!kbhit()) {
+        while (!_kbhit()) {
             if(/**clock() >= tempsaffmatrices && **/switchmatrice == 1){
                 system("clear");
 
@@ -697,7 +706,7 @@ coordonnees_tir Aim(char** matrice, short nb_colonnes, short nb_lignes){
         }
 
         //Boucle d'affichage des deux matrices pour clignotement
-        while (!kbhit()) {
+        while (!_kbhit()) {
             if(/**clock() >= tempsaffmatrices && **/switchmatrice == 1){
                 system("clear");
 
@@ -1116,4 +1125,23 @@ void libererMatrice(short** matrice, short nb_lignes) {
     //On libere le bloc memoire des pointeurs (premier tableau)
     free(matrice);
     matrice = NULL;
+}
+
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (! initialized) {
+        // Use termios to turn off line buffering
+        struct termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
 }
